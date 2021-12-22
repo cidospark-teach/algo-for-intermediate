59b3bb20fa44c7ae38a51a342b5a8e72505f4ddb
macbook <macbook@macbooks-MacBook-Pro.local>
2021-12-22 08:27:08 +0100

    search cleanPile

--- LaundryMachine/CoreSolution/ClassLib.cs
+++ LaundryMachine/CoreSolution/ClassLib.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CoreSolution
 {
-    public class Class1
+    public class ClassLib
     {
+        public int GetMaximumPairOfSocks(int noOfWashes, int[] cleanPile, int[] dirtyPile)
+        {
+            if (noOfWashes < 0 || noOfWashes > 50)
+                return -1;
+
+            if(cleanPile.Length < 1 || cleanPile.Length > 50)
+                return -1;
+
+            if (dirtyPile.Length < 1 || dirtyPile.Length > 50)
+                return -1;
+
+            var listOfCleanPile = new List<int>(cleanPile);
+            var listOfDirtyPile = new List<int>(dirtyPile);
+            listOfCleanPile.Sort();
+            listOfDirtyPile.Sort();
+            var maxNoOfWash = 0;
+
+            int index = 0;
+            while (index < listOfCleanPile.Count && (index + 1) < listOfCleanPile.Count)
+            {
+                if (listOfCleanPile[index] == listOfCleanPile[index + 1])
+                {
+                    maxNoOfWash += 1;
+                    listOfCleanPile.Remove(listOfCleanPile[index]);
+                    listOfCleanPile.Remove(listOfCleanPile[index]);
+                }
+                else
+                {
+                    index += 1;
+                }
+            }
+
+            return maxNoOfWash;
+        }
     }
 }
