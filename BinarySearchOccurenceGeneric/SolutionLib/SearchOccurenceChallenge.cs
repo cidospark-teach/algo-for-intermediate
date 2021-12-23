eeec42329a76c96d2fc9d714917aed1a57257e5e
macbook <macbook@macbooks-MacBook-Pro.local>
2021-12-24 00:49:53 +0100

    initial commit

---
+++ BinarySearchOccurenceGeneric/SolutionLib/SearchOccurenceChallenge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionLib
+{
+    public class SearchOccurenceChallenge<T>
+    {
+        public static T Search(T[] arr)
+        {
+            return default(T);
+        }
+
+        private static bool CompareList(List<T> list, T searchTerm)
+        {
+            return false;
+        }
+
+        private int getNumberOfOccurrence(List<T> list)
+        {
+            return 0;
+        }
+    }
+}
