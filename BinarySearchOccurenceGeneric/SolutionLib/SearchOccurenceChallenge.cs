84b1d609efac27e003809ea2ddd11ff72f43a483
cidospark-teach <engrcidos@outlook.com>
2021-12-24 01:46:45 +0100

    sorted the list, checked for search item match with middle item, first item and last item

--- BinarySearchOccurenceGeneric/SolutionLib/SearchOccurenceChallenge.cs
+++ BinarySearchOccurenceGeneric/SolutionLib/SearchOccurenceChallenge.cs
@@ -5,9 +5,36 @@ namespace SolutionLib
 {
     public class SearchOccurenceChallenge<T>
     {
-        public static T Search(T[] arr)
+        public static Dictionary<int, T> Search(T[] arr, T searchItem)
         {
-            return default(T);
+            // return early if array is empty
+            if (arr.Length <= 0)
+                return null;
+
+            // convert array to list and sort
+            var list = new List<T>(arr);
+            list.Sort();
+
+            // get middle index and middleItem
+            int middleIndex = list.Count / 2;
+            var middleItem = list[middleIndex];
+
+            // return result if middle Item match search item
+            if (searchItem.Equals(middleIndex))
+                return new Dictionary<int, T>() { { middleIndex, middleItem } };
+
+            // return result if first Item match search item
+            if (searchItem.Equals(list[0]))
+                return new Dictionary<int, T>() { { 0, list[0] } };
+
+            // return result if middleIndex match search item
+            if (searchItem.Equals(middleIndex))
+                return new Dictionary<int, T>() { { list.Count - 1, list[list.Count - 1] } };
+
+            
+
+
+            return null;
         }
 
         private static bool CompareList(List<T> list, T searchTerm)
