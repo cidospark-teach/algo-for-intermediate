bb948e05774962e02232d561dfb2457a1164feed
cidospark-teach <engrcidos@outlook.com>
2021-12-24 13:10:08 +0100

    added logic to print result

--- BinarySearchOccurenceGeneric/MainProj/Program.cs
+++ BinarySearchOccurenceGeneric/MainProj/Program.cs
@@ -10,13 +10,42 @@ namespace MainProj
         {
             var arr = new int[] { 4, 4, 13, 4, 4, 4 };
             var dict = new Dictionary<int, List<int>>();
-            int searchTerm = 13;
+            int searchTerm = 4;
 
+            // I did something a little bit extra here ==>
+            // this for loop is arranging the indexes of the arr items into value pairs of there frequency
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(dict.ContainsKey(arr[i]))
+                {
+                    dict[arr[i]].Add(i);
+                }
+                else
+                {
+                    dict.Add(arr[i], new List<int>(){ i });
+                }
+            }
 
             var res = SearchOccurenceChallenge<int>.Search(arr, searchTerm);
 
+
+            //- - - - PRINTING THE OUTPUT - - - - //
             Console.Write(res < 1 ? "Not found" : $"Found {res} occurence(s) for the search term ==> {searchTerm}");
 
+            if(res > 0)
+            {
+                Console.Write(" at the following index(es):  ");
+
+                var indexes = dict[searchTerm];
+
+                foreach(var item in indexes)
+                {
+                    Console.Write(item + ", ");
+                }
+
+                Console.WriteLine("\n");
+            }
+            //- - - - - - - - - - - -  - - - - - //
         }
     }
 }
